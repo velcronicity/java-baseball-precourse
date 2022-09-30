a5fcf32a64819349673e6147b0fef6ca30933487
velcronicity <rtbf0826@gmail.com>
2022-09-30 23:37:04 +0900

    test: 외부 모듈
    외부 모듈 확인 및 기능 테스트

---
+++ src/test/java/baseball/ExternalModuleTest.java
@@ -0,0 +1,21 @@
+package baseball;
+
+import static camp.nextstep.edu.missionutils.Randoms.*;
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ExternalModuleTest {
+
+    @DisplayName("랜덤값 추출 함수 테스트")
+    @Test
+    void pickNumberInRangeTest() {
+        for (int i = 0; i < 100; i++) {
+            assertThat(pickNumberInRange(1, 9)).isNotZero();
+        }
+        for (int i = 0; i < 100; i++) {
+            assertThat(pickNumberInRange(0, 0)).isZero();
+        }
+    }
+}
